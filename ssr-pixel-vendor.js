6cc9d1061f6c369b2347363371ee7c33bf195804
Isaias Tavares <isaiasengsoft@gmail.com>
2024-02-25 22:46:15 -0300

    Update ssr-pixel-vendor.js

--- ssr-pixel-vendor.js
+++ ssr-pixel-vendor.js
@@ -1,4 +1,3 @@
-const TIMEOUT_DURATION = 30000; // 30 seconds
 const SSR_ID_PARAM = 'ssr_id';
 const ENDPOINT_VENDOR_CTA = 'vendor-cta';
 const ENDPOINT_VENDOR_EMAIL = 'vendor-email';
@@ -28,26 +27,24 @@ function hasSsrId() {
     return window.location.search.includes(SSR_ID_PARAM);
 }
 
-setTimeout(function () {
-    if (hasSsrId()) {
-        const ssrId = new URLSearchParams(window.location.search).get(SSR_ID_PARAM);
+if (hasSsrId()) {
+    const ssrId = new URLSearchParams(window.location.search).get(SSR_ID_PARAM);
 
-        document.addEventListener('click', function (event) {
-            if (event.target.tagName === 'A' && event.target.href) {
-                sendEvent(ENDPOINT_VENDOR_CTA, { click_id: ssrId });
-            }
-        });
+    document.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A' && event.target.href) {
+            sendEvent(ENDPOINT_VENDOR_CTA, { ssr_id: ssrId });
+        }
+    });
 
-        document.addEventListener('input', function (event) {
-            if (event.target.matches('input[type="email"]')) {
-                sendEvent(ENDPOINT_VENDOR_EMAIL, { click_id: ssrId });
-            }
-        });
+    document.addEventListener('input', function (event) {
+        if (event.target.matches('input[type="email"]')) {
+            sendEvent(ENDPOINT_VENDOR_EMAIL, { ssr_id: ssrId });
+        }
+    });
 
-        document.addEventListener('submit', function (event) {
-            if (event.target.matches('form')) {
-                sendEvent(ENDPOINT_VENDOR_CONVERSION, { click_id: ssrId });
-            }
-        });
-    }
-}, TIMEOUT_DURATION);
+    document.addEventListener('submit', function (event) {
+        if (event.target.matches('form')) {
+            sendEvent(ENDPOINT_VENDOR_CONVERSION, { ssr_id: ssrId });
+        }
+    });
+}
