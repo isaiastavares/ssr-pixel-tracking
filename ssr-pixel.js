eb8c5ef428154d10ecdc117a0dadf9787afec874
Isaias Tavares <isaiasengsoft@gmail.com>
2024-03-26 18:20:00 -0300

    add ssr-pixel.js

---
+++ ssr-pixel.js
@@ -0,0 +1,19 @@
+const SSR_ID_PARAM = 'ssr_id';
+const SSR_PIXEL_ID = 'ssr-pixel';
+
+function hasSsrId() {
+    return window.location.search.includes(SSR_ID_PARAM);
+}
+
+function getSsrId() {
+  return new URLSearchParams(window.location.search).get(SSR_ID_PARAM);
+}
+
+window.onload = function() {
+    if (hasSsrId()) {
+      const ssrId = getSsrId();
+      var imgSrc = document.getElementById(SSR_PIXEL_ID).src;
+      imgSrc = imgSrc.replace('{{ssr_id}}', ssrId);
+      //document.getElementById(SSR_PIXEL_ID).src = imgSrc;
+    }
+};
